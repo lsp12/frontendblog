45f686e85e64afc1f8f273dd1fcf3a6596b89f1e
lsp12 <jonathankenny852@gmail.com>
2022-01-16 22:19:13 -0500

    min

--- src/shared/Controllers/Auth/Auth.ts
+++ src/shared/Controllers/Auth/Auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { IRegister } from '../../../Module/Register/interface/interface';
