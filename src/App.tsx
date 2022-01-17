98d6a470ce8dd95e48e23f1bcbb1fb381f89c934
lsp12 <jonathankenny852@gmail.com>
2022-01-16 22:37:32 -0500

    create

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import Cookies from 'js-cookie';
 import moment from 'moment';
 import AppRouter from './Router/AppRouter';
 import { getToken } from './shared/Store/ActionAuth/Auth.slice';
-import { useAppDispatch } from './shared/Store/Hook';
+import { useAppDispatch, useAppSelector } from './shared/Store/Hook';
 import { theme } from './shared/Thema/Theme';
 import { userReducer } from './shared/Store/ActionAuth/Auth.reducer';
 
 function App() {
+  const { authethicated } = useAppSelector(( state ) => state.authSlice );
   const dispatch = useAppDispatch();
   const token = Cookies.get( 'token' );
   useEffect(() => {
     dispatch( getToken());
     dispatch( userReducer());
-  }, [dispatch, token]);
+  }, [dispatch, token, authethicated]);
 
   moment.locale( 'es' );
   return (
